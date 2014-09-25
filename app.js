1a7402081d1603827de99f5c2f68e2dbd3d1ad42
dmitrii <55dmitrii@gmail.com>
2014-09-25 19:43:23 +0300

    add breakpoint err app

--- app.js
+++ app.js
@@ -16,6 +16,26 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 
+app.use(logErrors);
+app.use(clientErrorHandler);
+app.use(errorHandler);
+
+function logErrors(err, req, res, next) {
+  console.error(err.stack);
+  next(err);
+}
+function clientErrorHandler(err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send({ error: 'Something blew up!' });
+  } else {
+    next(err);
+  }
+}
+function errorHandler(err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err });
+}
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.all('/*', function(req, res, next) {
